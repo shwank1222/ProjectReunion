3618efd9b9fc9666207132dafd1015deb5ad570d
ttingpan <dx123xb@naver.com>
2026-07-23 14:55:27 +0900

    [Add] Character & Player Controller
    - 캐릭터 기본 클래스 / 플레이어 캐릭터 / 적 캐릭터 추가
    - 이동 및 시선 입력 적용
    - 불필요 에셋 제거(점프 입력 액션, 터치 관련 에셋 등)

---
+++ Source/OSMK/Character/OSMKPlayerController.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OSMKPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+class UEnhancedInputLocalPlayerSubsystem;
+
+AOSMKPlayerController::AOSMKPlayerController()
+{
+}
+
+void AOSMKPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (IsValid(PlayerCameraManager))
+	{
+		PlayerCameraManager->ViewPitchMin = - 70.0f;
+		PlayerCameraManager->ViewPitchMax = 80.0f;
+	}
+}
+
+void AOSMKPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	if (IsLocalPlayerController())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+		{
+			for (const UInputMappingContext* CurrentContext : DefaultMappingContexts)
+			{
+				Subsystem->AddMappingContext(CurrentContext, 0);
+			}
+		}
+	}
+}
---
+++ Source/OSMK/Character/PlayerCharacter.cpp
@@ -0,0 +1,76 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCharacter.h"
+
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+APlayerCharacter::APlayerCharacter()
+{
+	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
+	
+	FirstPersonMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("First Person Mesh"));
+
+	FirstPersonMesh->SetupAttachment(GetMesh());
+	FirstPersonMesh->SetOnlyOwnerSee(true);
+	FirstPersonMesh->FirstPersonPrimitiveType = EFirstPersonPrimitiveType::FirstPerson;
+	FirstPersonMesh->SetCollisionProfileName(FName("NoCollision"));
+	
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("First Person Camera"));
+	FirstPersonCameraComponent->SetupAttachment(FirstPersonMesh, FName("head"));
+	FirstPersonCameraComponent->SetRelativeLocationAndRotation(FVector(-2.8f, 5.89f, 0.0f), FRotator(0.0f, 90.0f, -90.0f));
+	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+	FirstPersonCameraComponent->bEnableFirstPersonFieldOfView = true;
+	FirstPersonCameraComponent->bEnableFirstPersonScale = true;
+	FirstPersonCameraComponent->FirstPersonFieldOfView = 70.0f;
+	FirstPersonCameraComponent->FirstPersonScale = 0.6f;
+	
+	GetMesh()->SetOwnerNoSee(true);
+	GetMesh()->FirstPersonPrimitiveType = EFirstPersonPrimitiveType::WorldSpaceRepresentation;
+
+	GetCapsuleComponent()->SetCapsuleSize(34.0f, 96.0f);
+	
+	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
+	GetCharacterMovement()->AirControl = 0.5f;
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayerCharacter::MoveInput);
+		
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APlayerCharacter::LookInput);
+	}
+}
+
+void APlayerCharacter::MoveInput(const FInputActionValue& Value)
+{
+	if (!GetController())
+	{
+		return;
+	}
+
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+	
+	AddMovementInput(GetActorRightVector(), MovementVector.X);
+	AddMovementInput(GetActorForwardVector(), MovementVector.Y);
+}
+
+void APlayerCharacter::LookInput(const FInputActionValue& Value)
+{
+	if (!GetController())
+	{
+		return;
+	}
+
+	const FVector2D LookVector = Value.Get<FVector2D>();
+	
+	AddControllerYawInput(LookVector.X);
+	AddControllerPitchInput(LookVector.Y);
+}
+
