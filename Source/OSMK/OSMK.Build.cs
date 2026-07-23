0f6a09ead692467902f995853303a1890a5cf689
KimSeungHwan <ksh951222@gmail.com>
2026-07-23 12:20:15 +0900

    [Add] Unreal Project

---
+++ Source/OSMK/OSMK.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class OSMK : ModuleRules
+{
+	public OSMK(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"OSMK",
+			"OSMK/Variant_Horror",
+			"OSMK/Variant_Horror/UI",
+			"OSMK/Variant_Shooter",
+			"OSMK/Variant_Shooter/AI",
+			"OSMK/Variant_Shooter/UI",
+			"OSMK/Variant_Shooter/Weapons"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/OSMK/OSMK.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OSMK.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OSMK, "OSMK" );
+
+DEFINE_LOG_CATEGORY(LogOSMK)
