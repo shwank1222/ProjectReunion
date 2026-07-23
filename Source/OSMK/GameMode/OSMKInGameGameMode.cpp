3428705c5b90912510cb547c7a6d12ae846c054e
leenaaq <v584731@gmail.com>
2026-07-23 22:15:15 +0900

    [Feat] Implement enemy spawn function

--- Source/OSMK/GameMode/OSMKInGameGameMode.cpp
+++ Source/OSMK/GameMode/OSMKInGameGameMode.cpp
@@ -1,5 +1,6 @@
 #include "GameMode/OSMKInGameGameMode.h"
 #include "UI/Scouting/ScoutingWidget.h"
+#include "Data/StageData.h"
 #include "Blueprint/UserWidget.h"
 
 void AOSMKInGameGameMode::BeginPlay()
@@ -16,4 +17,36 @@ void AOSMKInGameGameMode::BeginPlay()
 	{
 		ScoutingWidget->AddToViewport();
 	}
+}
+
+void AOSMKInGameGameMode::SpawnEnemies(int32 StageIndex)
+{
+	if (!StageData)
+	{
+		UE_LOG(LogTemp, Error, TEXT("[InGameGameMode] StageData is null"));
+		return;
+	}
+
+	if (!StageData->Stages.IsValidIndex(StageIndex))
+	{
+		UE_LOG(LogTemp, Error, TEXT("[InGameGameMode] Invalid StageIndex: %d"), StageIndex);
+		return;
+	}
+
+	UClass* EnemyClass = StageData->EnemyClass.LoadSynchronous();
+	if (!EnemyClass)
+	{
+		UE_LOG(LogTemp, Error, TEXT("[InGameGameMode] EnemyClass is null"));
+		return;
+	}
+
+	const TArray<FVector>& SpawnLocations = StageData->Stages[StageIndex].EnemySpawnLocations;
+	UWorld* World = GetWorld();
+
+	for (const FVector& Location : SpawnLocations)
+	{
+		World->SpawnActor<AActor>(EnemyClass, Location, FRotator::ZeroRotator);
+	}
+
+	UE_LOG(LogTemp, Log, TEXT("[InGameGameMode] Spawned %d enemies"), SpawnLocations.Num());
 }
--- Source/OSMK/GameMode/OSMKInGameGameMode.h
+++ Source/OSMK/GameMode/OSMKInGameGameMode.h
@@ -9,11 +9,18 @@ class OSMK_API AOSMKInGameGameMode : public AGameMode
 {
 	GENERATED_BODY()
 
+public:
+	UFUNCTION(BlueprintCallable)
+	void SpawnEnemies(int32 StageIndex);
+	
 protected:
 	virtual void BeginPlay() override;
 
 	UPROPERTY(EditDefaultsOnly, Category = "UI")
 	TSubclassOf<class UScoutingWidget> ScoutingWidgetClass = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Stage")
+	class UStageData* StageData = nullptr;
 
 private:
 	UPROPERTY()
