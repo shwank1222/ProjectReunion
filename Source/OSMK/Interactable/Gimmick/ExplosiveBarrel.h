46f84b13c66d58aa7a44b09009f411f98ad22428
KimSeungHwan <ksh951222@gmail.com>
2026-07-23 22:29:24 +0900

    [Add] Base Gimmick Class for prototype and Modules
    1. GimmickBase 클래스 생성
    - 프로토타입 기름통(ExplosiveBarrel) 클래스 추가
    
    2. 나이아가라, GeometryCollection, FieldSystemEngine 모듈 추가

---
+++ Source/OSMK/Interactable/Gimmick/ExplosiveBarrel.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "Interactable/Gimmick/GimmickBase.h"
+#include "ExplosiveBarrel.generated.h"
+
+class UStaticMeshComponent;
+class UNiagaraSystem;
+class USoundBase;
+
+UCLASS()
+class OSMK_API AExplosiveBarrel : public AGimmickBase
+{
+	GENERATED_BODY()
+
+public:
+	AExplosiveBarrel();
+	
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UStaticMeshComponent> BarrelMesh;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UGeometryCollectionComponent> GeometryCollection;
+	
+	virtual void OnTriggered() override;
+	
+	void Explode();
+
+	void ScanExplosionRadius();
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, Category="Explosion")
+	float ExplosionImpulse = 1500.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Explosion")
+	float ExplosionRadius = 500.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Explosion")
+	float ExplosionDelay = 1.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Explosion")
+	bool bChainReaction = true;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Explosion")
+	TObjectPtr<UNiagaraSystem> ExplosionEffect;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Explosion")
+	TObjectPtr<USoundBase> ExplosionSound;
+
+	UPROPERTY(EditAnywhere, Category="Explosion")
+	bool bShowExplosionDebug = true;
+private:
+	FTimerHandle ExplosionTimerHandle;
+	
+	
+};
