c15382c535aad191ad935bb35c395740f8454285
leenaaq <v584731@gmail.com>
2026-07-26 05:13:57 +0900

    [Feat] implement in-game revolver cylinder animation

--- Source/OSMK/UI/Ingame/IngameBulletSlotWidget.h
+++ Source/OSMK/UI/Ingame/IngameBulletSlotWidget.h
@@ -15,6 +15,7 @@ class OSMK_API UIngameBulletSlotWidget : public UUserWidget
 public:
 	virtual void NativeConstruct() override;
 	virtual void NativeDestruct() override;
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
 
 	UFUNCTION()
 	void UpdateBullets();
@@ -44,6 +45,12 @@ protected:
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UImage> BulletSlot_6 = nullptr;
 
+	UPROPERTY(meta = (BindWidget))
+	class UCanvasPanel* CylinderPanel = nullptr;
+	
 private:
 	TWeakObjectPtr<APlayerCharacter> CachedPlayerCharacter = nullptr;
+	float CurrentRotationAngle = 0.0f;
+	float TargetRotationAngle = 0.0f;
+	int32 PreviousAmmoCount = 0;
 };
